a53f40764ba85c193345967c92e13fe53eed9c28
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-02-13 19:06:08 +0600

    first commit

---
+++ src/app/components/HomeWrapper.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeWrapper = () => {
+  return (
+    <>
+      <section className="h-screen">Hero</section>
+      <section className="h-screen">Parallax</section>
+      <section className="h-screen">Service</section>
+      <section className="h-screen">Portfolio1</section>
+      <section className="h-screen">Portfolio2</section>
+      <section className="h-screen">Portfolio3</section>
+      <section className="h-screen">Contact</section>
+    </>
+  );
+};
+
+export default HomeWrapper;
